b3d76d8426eb21bd50c82ca5c6b37867d7612c7d
Ishan Buddhika <ishan@thesanmark.com>
2016-07-14 14:21:28 +0530

    pass properties to view with {{}}

--- app/main.js
+++ app/main.js
@@ -1,7 +1,5 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_component_1 = require('./app.component');
-var app2_component_1 = require('./app2.component');
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
-platform_browser_dynamic_1.bootstrap(app2_component_1.App2Component);
 //# sourceMappingURL=main.js.map
