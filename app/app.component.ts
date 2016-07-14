0d6daa40e012739e193727905a29c188ae9b3377
Ishan Buddhika <ishan@thesanmark.com>
2016-07-14 12:33:38 +0530

    Use first component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
+import {App2Component} from './app2.component';
+
 @Component({
-  selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+	selector: 'my-app',
+	template: '<h1>My First Angular 2 App</h1><my-app2></my-app2>',
+	directives: [
+		App2Component
+	]
 })
 export class AppComponent { }
---
+++ app/app2.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app2',
+  template: '<h1>My First Angular 2 App 2</h1>'
+})
+export class App2Component { }
